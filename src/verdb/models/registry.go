509d4636c4904b5f6deb45424be7baf39a415971
Alex Chen <alexanderchenmh@gmail.com>
2015-05-12 19:52:33 +0800

    添加注册模型

---
+++ src/verdb/models/registry.go
@@ -0,0 +1,190 @@
+package models
+
+import (
+	"reflect"
+	"strings"
+	"sync"
+	"time"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	// Hourly 一个小时包含的秒数
+	Hourly = 60 * 60
+	// Daily 一天包含的秒数
+	Daily = 24 * Hourly
+	// Weekly 一个星期包含的秒数
+	Weekly = 7 * Daily
+	// Monthly 一个月包含的秒数
+	Monthly = 30 * Daily
+)
+
+/*
+Registry 注册模型
+{
+	"databaseName" : "frradar", // 目标数据库
+	"collectionName" : "serverInfo", // 目标集合
+	"name" : "frradar/serverInfo", // 命名方式： databaseName/collectionName
+	"compareKey" : "serverId", // 用来标识同一个实体
+	'verInterval': 24 * 60 * 60, // 版本粒度，示例为一天记录一个版本
+	"indexKeys" : [ // 需要添加index的键
+		"xxx.xxx",
+		"xxx.xxxxx",
+		...
+	],
+	"verKeys" : [ // 需要版本化记录的建
+		"xxx.xxx",
+		"xx.xxx.xx",
+		...
+	]
+}
+*/
+type Registry struct {
+	sync.Mutex
+	DatabaseName   string
+	CollectionName string
+	Name           string
+	CompareKey     string
+	VerInterval    int64
+	IndexKeys      []string
+	VerKeys        []string
+}
+
+// GenVer 基于VerInterval生成版本号: unix seconds / interval
+func (reg *Registry) GenVer() int64 {
+	if reg.VerInterval <= 0 { // 用于测试时生成新版本
+		return time.Now().UnixNano()
+	}
+	return int64(time.Now().Second()) / reg.VerInterval
+}
+
+/*
+Versionize 版本化记录数据
+1. 对于提交的记录，添加_ver,_next生成新版本new
+2. new的格式是{CompareKey: <int|string>, _ver: <int>, _next: <int>} 且 new._ver 相同于 new._next
+3. 在数据库中查询相同CompareKey，且_next最大的版本old
+4. 如果 old._ver 相同于 new._ver
+	* 用new的内容跟新old，然后返回
+5. 如果 old 相同于 new
+	* 更新old版本的_next为new的_ver，然后返回
+6 如果 old 不同于 new
+	* 更新old版本的_next为new的_ver
+	* 插入new，然后返回
+*/
+func (reg *Registry) Versionize(newDoc map[string]interface{}, sess *mgo.Session) error {
+	reg.Lock()
+	defer reg.Unlock()
+
+	// 新建记录添加版本信息
+	ver := reg.GenVer()
+	newDoc["_ver"] = ver
+	newDoc["_next"] = ver
+	newDoc["_is_latest"] = true
+
+	// 记录存储表
+	collection := sess.DB(reg.DatabaseName).C(reg.CollectionName)
+
+	// 查询表中同一实例最新的记录
+	var oldDoc map[string]interface{}
+	err := collection.Find(bson.M{
+		reg.CompareKey: newDoc[reg.CompareKey],
+		"_is_latest":   true,
+	}).One(&oldDoc)
+
+	// 如果没有找到记录，表面新记录是第一个版本
+	if err == mgo.ErrNotFound {
+		return collection.Insert(newDoc)
+	} else if err != nil {
+		return err
+	}
+
+	// 如果提交的记录和数据库中最新记录在同一个Interval中，用提交记录的信息更新数据库中的最新记录
+	if oldDoc["_ver"] == newDoc["_ver"] {
+		return collection.UpdateId(oldDoc["_id"], newDoc)
+	}
+
+	// 如果提交的记录和数据库中的记录内容一致，更新数据库中记录_next，同时用提交数据的内容更新数据库记录
+	if !changed(oldDoc, newDoc, reg.VerKeys) {
+		setMap := bson.M{"_next": ver}
+		for k, v := range newDoc {
+			// 屏蔽键： "_ver", "_next", "_id", "_is_latest"
+			if len(k) > 0 && k[0] == '_' {
+				continue
+			}
+			setMap[k] = v
+		}
+		return collection.UpdateId(
+			oldDoc["_id"],
+			bson.M{"$set": setMap},
+		)
+	}
+
+	if err = collection.UpdateId(
+		oldDoc["_id"],
+		bson.M{"$set": bson.M{
+			"_next":      ver - 1,
+			"_is_latest": false,
+		}},
+	); err != nil {
+		return err
+	}
+	return collection.Insert(newDoc)
+}
+
+// 比对两条记录，keys对应的值有没有改变。
+// 比对两条记录时，如果键对应的值是列表，要求列表内容有稳定性。
+// 也就是俩个高列表里面如果内容一致，但是顺序不一致会认为是不一样的列表。
+func changed(ldoc, rdoc map[string]interface{}, keys []string) bool {
+	for _, key := range keys {
+		parts := strings.Split(key, ".")
+		va := collectVals(ldoc, parts)
+		vb := collectVals(rdoc, parts)
+		if !reflect.DeepEqual(va, vb) {
+			return true
+		}
+	}
+	return false
+}
+
+// 从记录中收集所有keys对应的值
+func collectVals(m map[string]interface{}, parts []string) []interface{} {
+	var nexts = []interface{}{m}
+	var vals []interface{}
+	for i, k := range parts {
+		var nnexts []interface{}
+		for _, next := range nexts {
+			switch tnext := next.(type) {
+			case map[string]interface{}:
+				if tnext[k] == nil {
+					continue
+				}
+				if i == len(parts)-1 {
+					vals = append(vals, tnext[k])
+				} else {
+					nnexts = append(nnexts, tnext[k])
+				}
+
+			case []interface{}:
+				for _, v := range tnext {
+					if tm, ok := v.(map[string]interface{}); ok {
+						if tm[k] == nil {
+							continue
+						}
+						if i == len(parts)-1 {
+							vals = append(vals, tm[k])
+						} else {
+							nnexts = append(nnexts, tm[k])
+						}
+					}
+				}
+			}
+		}
+		if len(nnexts) == 0 {
+			break
+		}
+		nexts = nnexts
+	}
+	return vals
+}
---
+++ src/verdb/models/registry_test.go
@@ -0,0 +1,233 @@
+package models
+
+import (
+	"encoding/json"
+	"math/rand"
+	"reflect"
+	"testing"
+
+	"gopkg.in/mgo.v2"
+)
+
+func TestVersionize(t *testing.T) {
+	// 需要测试的新版本数
+	num := 100
+
+	// 基础记录
+	baseJSON := `
+	{
+		"pk": 1,
+		"a": 1,
+		"b": {
+			"c": 1
+		},
+		"d": [
+			{"e": 1,  "i": 2},
+			{"e": 2, "i": 3}
+		],
+		"f": [
+			{"g": [{"h": 1}, {"h": 2}]}
+		],
+		"j": {
+			"k": 1
+		}
+	}`
+	var base map[string]interface{}
+	json.Unmarshal([]byte(baseJSON), &base)
+	/*
+		change:
+			versionized:
+				a
+				b.c
+				d.e
+				d.i
+				f.g.h
+		updated:
+			j.k
+
+	*/
+	// 基础记录修改函数
+	var count = 100
+	var getCount = func() int {
+		count++
+		return count
+	}
+	var changeA = func(doc map[string]interface{}) map[string]interface{} {
+		doc["a"] = getCount()
+		return doc
+	}
+	var changeBC = func(doc map[string]interface{}) map[string]interface{} {
+		doc["b"].(map[string]interface{})["c"] = getCount()
+		return doc
+	}
+	var changeDE = func(doc map[string]interface{}) map[string]interface{} {
+		for _, item := range doc["d"].([]interface{}) {
+			item.(map[string]interface{})["e"] = getCount()
+		}
+		return doc
+	}
+	var changeDI = func(doc map[string]interface{}) map[string]interface{} {
+		for _, item := range doc["d"].([]interface{}) {
+			item.(map[string]interface{})["i"] = getCount()
+		}
+		return doc
+	}
+	var changeFGH = func(doc map[string]interface{}) map[string]interface{} {
+		for _, item := range doc["f"].([]interface{}) {
+			for _, nitem := range item.(map[string]interface{})["g"].([]interface{}) {
+				nitem.(map[string]interface{})["h"] = getCount()
+			}
+		}
+		return doc
+	}
+	var changeJK = func(doc map[string]interface{}) map[string]interface{} {
+		doc["j"].(map[string]interface{})["k"] = getCount()
+		return doc
+	}
+
+	// 生成新版本修改函数列表
+	changeLst := []func(map[string]interface{}) map[string]interface{}{changeA, changeBC, changeDE, changeDI, changeFGH}
+
+	// 更新最新记录函数列表
+	updateLst := []func(map[string]interface{}) map[string]interface{}{changeJK}
+
+	// 从函数列表中随机选择一些函数
+	randSelect := func(fns []func(map[string]interface{}) map[string]interface{}) []func(map[string]interface{}) map[string]interface{} {
+		size := len(fns)
+		var nfns []func(map[string]interface{}) map[string]interface{}
+		for i := 0; i < 1+rand.Intn(size); i++ {
+			nfns = append(nfns, fns[rand.Intn(size)])
+		}
+		return nfns
+	}
+
+	// registery
+	regJSON := `
+	{
+	    "databaseName": "testdb",
+	    "compareKey": "pk",
+	    "collectionName": "testver",
+	    "indexKeys": [
+	        ["a"],
+	        ["b.c"],
+	        ["d.e"],
+	        ["d.i"],
+	        ["f.g.h"]
+	    ],
+	    "verKeys": [
+			"a",
+			"b.c",
+			"d.e",
+			"d.i",
+			"f.g.h"
+	    ]
+    }`
+
+	var reg = &Registry{}
+	json.Unmarshal([]byte(regJSON), reg)
+
+	// 链接数据库
+	sess, _ := mgo.Dial("localhost")
+
+	// 清空数据库
+	sess.DB(reg.DatabaseName).C(reg.CollectionName).DropCollection()
+
+	rmVerInfo := func(doc map[string]interface{}) {
+		for k := range doc {
+			if len(k) > 0 && k[0] == '_' {
+				delete(doc, k)
+			}
+		}
+	}
+
+	findLast := func(sess *mgo.Session, reg *Registry) map[string]interface{} {
+		var last map[string]interface{}
+		sess.DB(reg.DatabaseName).C(reg.CollectionName).Find(
+			map[string]interface{}{
+				"_is_latest":   true,
+				reg.CompareKey: base[reg.CompareKey],
+			},
+		).One(&last)
+		return last
+	}
+
+	countNum := func(sess *mgo.Session, reg *Registry) int {
+		num, _ := sess.DB(reg.DatabaseName).C(reg.CollectionName).Count()
+		return num
+	}
+
+	// prettyJSON := func(data interface{}) string {
+	// 	s, _ := json.MarshalIndent(data, "", "\t")
+	// 	return string(s)
+	// }
+
+	// 测试插入第一个记录
+	reg.Versionize(base, sess)
+	if countNum(sess, reg) != 1 {
+		t.Errorf("插入第一条记录错误\n")
+		return
+	}
+
+	// 生成num个新版
+	for i := 0; i < num; i++ {
+		// 生成几个新版本，但是修改的key不需要生成新版本，只需要更新数据库中最新的记录
+		for j := 0; j < 1+rand.Intn(6); j++ {
+			for _, fn := range randSelect(updateLst) {
+				base = fn(base)
+			}
+			base["other_key"] = i + j
+			count := countNum(sess, reg)
+			oldLast := findLast(sess, reg)
+			reg.Versionize(base, sess)
+			last := findLast(sess, reg)
+			if !(oldLast["_next"].(int64) <= last["_next"].(int64)) {
+				t.Errorf("非版本化键修改，插入版本时未更新_next\n")
+				return
+			}
+			if oldLast["_ver"].(int64) != last["_ver"].(int64) {
+				t.Errorf("非版本化键修改，更新了版本\n")
+				return
+			}
+			rmVerInfo(base)
+			rmVerInfo(last)
+			if !reflect.DeepEqual(base, last) {
+				t.Errorf("非版本化键修改，插入版本时更新错误\n %#v \n %#v\n", (base), (last))
+				return
+			}
+			if count != countNum(sess, reg) {
+				t.Errorf("非版本化键修改，版本数据不对\n")
+				return
+			}
+		}
+
+		// 添加新版本
+		for _, fn := range randSelect(changeLst) {
+			base = fn(base)
+		}
+		for _, fn := range randSelect(updateLst) {
+			base = fn(base)
+		}
+
+		count := countNum(sess, reg)
+		oldLast := findLast(sess, reg)
+		reg.Versionize(base, sess)
+		last := findLast(sess, reg)
+		if !(oldLast["_next"].(int64) <= last["_next"].(int64)) {
+			t.Errorf("版本化键修改，插入版本时未更新_next\n")
+		}
+		if oldLast["_ver"].(int64) == last["_ver"].(int64) {
+			t.Errorf("版本化键修改，未生成版本\n")
+		}
+		if last["_ver"].(int64) != last["_next"].(int64) {
+			t.Errorf("版本化键修改，_ver 不等于 _next\n")
+		}
+		rmVerInfo(last)
+		rmVerInfo(base)
+		if !reflect.DeepEqual(base, last) {
+			t.Errorf("版本化键修改，插入新版本时更新错误\n")
+		}
+		if count+1 != countNum(sess, reg) {
+			t.Errorf("版本化键修改，版本数目不对\n")
+		}
+	}
+}
