b7f2ba7994c30c2e8d1a36837475bc1acb54a8d5
Alex Chen <alexanderchenmh@gmail.com>
2015-06-13 09:52:52 +0800

    简化代码

--- src/verdb/api/registry_handler.go
+++ src/verdb/api/registry_handler.go
@@ -3,24 +3,22 @@ package api
 import (
 	"verdb/models"
 
+	"gopkg.in/mgo.v2"
+
 	"github.com/gin-gonic/gin"
 )
 
 // 新建：POST /api/registry
 func NewRegistry(c *gin.Context) {
-	sess, err := getDBSession(c.Get("sess"))
-	if err != nil {
-		jsonError(c, err)
-		return
-	}
-	rm, err := getRegManager(c.Get("rm"))
-	if err != nil {
-		jsonError(c, err)
-		return
-	}
+	sess := c.MustGet("sess").(*mgo.Session)
+	rm := c.MustGet("rm").(*models.RegManager)
+
 	var reg models.Registry
 	c.Bind(&reg)
+
 	var nreg *models.Registry
+	var err error
+
 	if nreg, err = rm.CreateRegistry(&reg, sess); err != nil {
 		jsonError(c, err)
 		return
@@ -30,19 +28,15 @@ func NewRegistry(c *gin.Context) {
 
 // 修改：PUT /api/registry/:id
 func UpdateRegistry(c *gin.Context) {
-	sess, err := getDBSession(c.Get("sess"))
-	if err != nil {
-		jsonError(c, err)
-		return
-	}
-	rm, err := getRegManager(c.Get("rm"))
-	if err != nil {
-		jsonError(c, err)
-		return
-	}
+	sess := c.MustGet("sess").(*mgo.Session)
+	rm := c.MustGet("rm").(*models.RegManager)
+
 	var reg models.Registry
 	c.Bind(&reg)
+
 	var nreg *models.Registry
+	var err error
+
 	if nreg, err = rm.UpdateRegistry(c.Params.ByName("id"), &reg, sess); err != nil {
 		jsonError(c, err)
 		return
@@ -60,40 +54,28 @@ func UpdateRegistry(c *gin.Context) {
 	}
 */
 func SearchRegistry(c *gin.Context) {
-	sess, err := getDBSession(c.Get("sess"))
-	if err != nil {
-		jsonError(c, err)
-		return
-	}
-	rm, err := getRegManager(c.Get("rm"))
-	if err != nil {
-		jsonError(c, err)
-		return
-	}
+	sess := c.MustGet("sess").(*mgo.Session)
+	rm := c.MustGet("rm").(*models.RegManager)
 	var obj models.SearchStruct
 	c.Bind(&obj)
 
 	var regs []models.Registry
+	var err error
+
 	if regs, err = rm.SearchRegistries(&obj, sess); err != nil {
 		jsonError(c, err)
 		return
 	}
+
 	jsonOk(c, regs)
 }
 
 // 删除：DELETE /api/registry/:id
 func DeleteRegistry(c *gin.Context) {
-	sess, err := getDBSession(c.Get("sess"))
-	if err != nil {
-		jsonError(c, err)
-		return
-	}
-	rm, err := getRegManager(c.Get("rm"))
-	if err != nil {
-		jsonError(c, err)
-		return
-	}
+	sess := c.MustGet("sess").(*mgo.Session)
+	rm := c.MustGet("rm").(*models.RegManager)
 	var reg *models.Registry
+	var err error
 
 	if reg, err = rm.DeleteRegistry(c.Params.ByName("id"), sess); err != nil {
 		jsonError(c, err)
--- src/verdb/api/util.go
+++ src/verdb/api/util.go
@@ -1,37 +1,12 @@
 package api
 
 import (
-	"errors"
 	"net/http"
-	"verdb/models"
 
 	"github.com/gin-gonic/gin"
 
-	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
-
-func getDBSession(sess interface{}, err error) (*mgo.Session, error) {
-	if err != nil {
-		return nil, err
-	}
-	_sess, ok := sess.(*mgo.Session)
-	if !ok {
-		return nil, errors.New("interface{} cant convert to *mgo.Session")
-	}
-	return _sess, nil
-}
-
-func getRegManager(rm interface{}, err error) (*models.RegManager, error) {
-	if err != nil {
-		return nil, err
-	}
-	_rm, ok := rm.(*models.RegManager)
-	if !ok {
-		return nil, errors.New("interface{} cant convert to *RegManger")
-	}
-	return _rm, nil
-}
 
 func jsonError(c *gin.Context, err error) {
 	c.JSON(http.StatusInternalServerError, bson.M{"status": "error", "msg": err.Error()})
