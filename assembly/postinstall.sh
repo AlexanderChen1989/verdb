2f5aa86dc04b42d44a8ae64c7fe838f8ef9df7fe
lizhongdang@idcos.com <lizhongdang@idcos.com>
2015-09-30 19:46:37 +0800

    RPM打包

--- assembly/postinstall.sh
+++ assembly/postinstall.sh
@@ -1,3 +1,3 @@
 chmod 755 /usr/yunji/idcos-collect-verdb/bin/verdb-server
 chown root:root /usr/yunji/idcos-collect-verdb/bin/verdb-server
-ln -s /usr/yunji/idcos-cli/bin/verdb-server /usr/local/bin/verdb-server
+ln -s /usr/yunji/idcos-collect-verdb/bin/verdb-server /usr/local/bin/verdb-server
