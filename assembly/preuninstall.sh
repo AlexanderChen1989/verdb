631f1bdb0304cc4048a00be924637e2cdce49ffc
lizhongdang@idcos.com <lizhongdang@idcos.com>
2015-09-30 19:07:34 +0800

    RPM打包

---
+++ assembly/preuninstall.sh
@@ -0,0 +1,4 @@
+if [ "$1" = "0" ]; then
+    unlink /usr/local/bin/verdb-server
+    rm -rf /usr/yunji/idcos-collect-verdb
+    
---
+++ assembly/rpm.sh
@@ -0,0 +1,22 @@
+#首先打包引用
+
+echo -------------------------------------------------------------------------
+echo idcos-cli rpm 打包工具
+echo -------------------------------------------------------------------------
+
+#初始目录
+CURRENT_DIR=`pwd`
+
+#返回根目录
+cd $CURRENT_DIR/..
+
+#编译
+. /etc/profile
+gb build
+
+#建立rpm包结构目录
+rm -rfd temp
+mkdir -p temp/bin
+
+#copy打包之后的代码
+cp bin/server temp/bin/verdb-server
